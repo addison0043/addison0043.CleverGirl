03dfcd321ede7f891c53eb4813cec6991fb5ba73
Addison Albrecht <addisonalbrecht@Addisons-MacBook-Air.local>
2018-01-18 16:48:26 -0600

    Added comments

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+// Audio files 
 var jurassMusic = new Audio("Assets/JurassicParkRemix.mp3");
 var growl = new Audio("Assets/growl.mp3");
+
+// Random number for velociraptor health
 var velocHealth = Math.floor(Math.random() * 40) + 10;
 	console.log(velocHealth);
+
+// Random number for T-Rex health
 var trex_health = Math.floor(Math.random() * 50) + 300;
 var rand;
 
+// Plays music and starts game
 window.onload = function() {
 	jurassMusic.play();
 	start_game();
 }
 
 
+// Choose a velociraptor. 3 choices.
 function start_game() {
 	$("#ceiling2").hide();
 	$("#subtext").html("Choose a Velociraptor");
@@ -19,6 +26,8 @@ function start_game() {
 	$("#delta").html('<img src="Assets/Delta.gif"/>');
 	$("#echo").html('<img src="Assets/Echo.gif"/>');
 
+	
+	// Generates red border around whichever velociraptor the user clicks
 	$('.veloc').click(function() {
 		
 		if ($(this).hasClass('velocBord')) {
@@ -30,6 +39,7 @@ function start_game() {
 	});    	
 }	
 
+// Once user chooses velociraptor
 function submit() {
 	
 	growl.play();
@@ -54,6 +64,7 @@ function submit() {
 		}
 }
 
+// Hide Grid 1, show Grid 2
 function hideVeloc() {
 	$("#ceiling").remove();
 	$("#ceiling2").show();
--- script2.js
+++ script2.js
@@ -1,7 +1,13 @@
+// Plays whenever you attack the 'peopleToEat'
 var attackSound = new Audio("Assets/attackSound.mp3");
+
+// Plays if you defeat T-Rex
 var winner = new Audio("Assets/winner.mp3");
+
+// Plays if you lose to T-Rex
 var tRexRoar = new Audio("Assets/tRexRoar.mp3");
 
+// Picture show before velociraptor "attacks"
 var peopleToEat = [
 	"Assets/children.jpg",
 	"Assets/claire.jpg",
@@ -12,6 +18,7 @@ var peopleToEat = [
 	"Assets/goat.jpg"
 ]
 
+// Shows after velociraptor "attacks"
 var peopleEaten = [
 	"Assets/children_eaten.jpg",
 	"Assets/claire_eaten.jpg",
@@ -22,8 +29,10 @@ var peopleEaten = [
 	"Assets/goat_eaten.jpg"
 ]
 
+// When you click "Attack"
 function eatPerson() {
 	
+	// Keeps running through peopleToEat array as long as it's length is greater than 4
 	if (peopleToEat.length > 4) {
 		
 		$("#next").hide();
@@ -39,24 +48,36 @@ function eatPerson() {
 		$("#vh").html(velocHealth);
 	}
 
+	// Fight T-Rex
 	else {
 		final_boss();
 	}
 }
 
+// Attack the peopleToEat 
 function attack() {
 	attackSound.play();
+	
+	// Adds to your health score
 	var addHealth = Math.floor(Math.random() * 100) + 60;
 	velocHealth += addHealth;
 	$("#vh").html(velocHealth);
+
+	// Displays new image after you attack
 	$("#twotwo").html("<img src='" + peopleEaten[rand] + "' />");
+	
+	// Removes person from array
 	peopleEaten.splice(rand, 1);
-	console.log(peopleEaten);
-	$("#next").show();
+
+	// Shows 'Next' button to keep moving through game
 	$("#attack").hide();
+	$("#next").show();	
 }
 
+// Fight T-Rex
 function final_boss() {
+	
+
 	$("#twotwo").empty();	
 	$("#next").hide();
 	$("#attack").hide();
@@ -65,22 +86,31 @@ function final_boss() {
 	$("#subtext").text("Attack the T-Rex!");
 	$("#twotwo").append('<img src="Assets/t-rex.jpg"/>');
 	
+		// Dynamically generate figcaption showing T-Rex's health
 		$("#twotwo").append('<figcaption class="health" id="tRexHealth">Health: <span id="trh"></span></figcaption>');
 
+		// Appends T-Rex's Health to DOM
 	$("#trh").text(trex_health);
 }
 
 function attack_rex() {
+	
+	// Damage inflicted to T-Rex 
 	var attack = Math.floor(Math.random() * 75) + 25;
 	trex_health -= attack;
 	console.log("Attack: " + attack + "; Health remaining: " + trex_health)
 	$("#trh").text(trex_health);
+
+	// Damage T-Rex inflicts on user's velociraptor
 	var attackBack = Math.floor(Math.random() * 150);
 	velocHealth -= attackBack;
 	console.log("Attack Back: " + attackBack + "; Health remaining: " + velocHealth)
 	$("#vh").text(velocHealth);
+	
+	// Recall the function until someone's health score gets below 0
 	final_boss();
 
+		// If velociraptor has health above 0 and T-rex doesn't, velociraptor wins
 		if (trex_health <= 0 && velocHealth > 0) {			
 			winner.play();
 			$("#onetwo").html('<img src="Assets/winner.jpg"/>');
@@ -93,6 +123,7 @@ function attack_rex() {
 			$("#reset").show();
 		}
 
+		// If T-Rex has health above 0 and velociraptor doesn't, T-Rex wins
 		else if (trex_health >= 0 && velocHealth <= 0) {
 			tRexRoar.play();
 			$("#subtext").hide();
@@ -104,6 +135,7 @@ function attack_rex() {
 			$("#reset").show();
 		}
 
+		// If T-Rex and velocirpators health simultaneously dip below 0, T-Rex wins
 		else if (trex_health <= 0 && velocHealth <= 0) {
 			tRexRoar.play();
 			$("#subtext").hide();
